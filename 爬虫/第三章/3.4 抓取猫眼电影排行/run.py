8693e42b1dcd4101db6a50ed37c3a7a1fd161bd1
hamdi <javs_shao@163.com>
2018-11-21 21:34:01 +0800

    提取我们想要的内容

--- 爬虫/第三章/3.4 抓取猫眼电影排行/run.py
+++ 爬虫/第三章/3.4 抓取猫眼电影排行/run.py
@@ -1,16 +1,31 @@
+import json
 import requests
+from requests.exceptions import RequestException
+import re
+import time
 
 
 def get_one_page(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    return '没有找到'
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
 
-def main():
-    url = 'http://maoyan.com/board/4'
-    html = get_one_page(url)
-    print(html)
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index':item[0],
+            'image':item[1],
+            'title':item[2],
+            'actor':item[3].strip()[3:],
+            'time':item[4].strip()[5:],
+            'score':item[5] + item[6],
+        }
 
-if __name__ == '__main__':
-    main()
