76806f82b6a71315bee8baae49fab1a8c67ef40c
hamdi <javs_shao@163.com>
2018-11-22 13:31:57 +0800

    属性多值匹配_运算符

--- 爬虫/第四章/4.1 XPath/7.属性多值匹配.py
+++ 爬虫/第四章/4.1 XPath/7.属性多值匹配.py
@@ -2,11 +2,11 @@ from lxml import etree
 
 
 text = '''
-    <li class="li li-first"><a href="link.html">first item</a></li>
+    <li class="li li-first" name="item"><a href="link.html">first item</a></li>
 '''
 
 html = etree.HTML(text)
 # result = html.xpath('//li[@class="li"]/a/text()')
 # result = html.xpath('//li[@class="li li-first"]/a/text()')
-result = html.xpath('//li[contains(@class, "li-first")]/a/text()')
+result = html.xpath('//li[contains(@class, "li-first") and @name="item"]/a/text()')
 print(result)
