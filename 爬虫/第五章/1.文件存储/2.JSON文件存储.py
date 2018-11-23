9465ed2dc77e60b3f6d0830a5e8169813193fda3
hamdi <javs_shao@163.com>
2018-11-23 16:34:39 +0800

    输出JSON

--- 爬虫/第五章/1.文件存储/2.JSON文件存储.py
+++ 爬虫/第五章/1.文件存储/2.JSON文件存储.py
@@ -24,3 +24,13 @@ print(data[0].get('name'))
 # 没有age属性，所以会报错，解决方式是：使用get来获取属性值
 # print(data[0]['age'])
 print(data[0].get('age'))   # 使用get方法获取属性值，如果没有该属性，那么不会报错，会返回None
+
+
+print('------输出JSON-----')
+data = [{
+    'name':'Bob',
+    'gender':'male',
+    'birthday':'1992-10-18'
+}]
+with open('data.json','a') as file:
+    file.write(json.dumps(data))
