69845485f87a2e930fa5ae04a8e52f8815dc4d0f
hamdi <javs_shao@163.com>
2018-11-25 20:09:35 +0800

    测试1

--- 爬虫/第五章/1.文件存储/2.JSON文件存储.py
+++ 爬虫/第五章/1.文件存储/2.JSON文件存储.py
@@ -38,3 +38,4 @@ print('------输出JSON-----')
 # }]
 with open('data.json','w') as file:
     file.write(json.dumps(data, indent=2, ensure_ascii=False))
+    file.write(json.dumps(data, indent=2, ensure_ascii=False))
