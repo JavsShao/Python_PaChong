6a115acdad3e260d82fb176054ded5a9f70db1ec
hamdi <javs_shao@163.com>
2018-11-26 09:48:09 +0800

    追加数据

--- 爬虫/第五章/1.文件存储/3.CSV文件存储.py
+++ 爬虫/第五章/1.文件存储/3.CSV文件存储.py
@@ -1,10 +1,19 @@
+# import csv
+
+
+# with open('data.csv', 'w') as csvfile:
+#     writer = csv.writer(csvfile)
+#     writer.writerow(['id','name','age'])
+#     writer.writerow(['10001','Mike',20])
+#     writer.writerow(['10002','Bob', 22])
+#     writer.writerow(['10003','Jordan',21])
+
+
+# 追加写入
 import csv
 
 
-with open('data.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(['id','name','age'])
-    writer.writerow(['10001','Mike',20])
-    writer.writerow(['10002','Bob', 22])
-    writer.writerow(['10003','Jordan',21])
-
+with open('data.csv','a') as csvfile:
+    fieldnames = ['id','name','age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerow({'id':'10004','name':'Durant','age':22})
