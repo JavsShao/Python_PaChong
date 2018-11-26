f841414be94b1c648b323c617af757ff6d74b6a9
hamdi <javs_shao@163.com>
2018-11-26 12:04:14 +0800

    插入数据_以字典传入

--- 爬虫/第五章/2.关系型数据库存储/3.插入数据.py
+++ 爬虫/第五章/2.关系型数据库存储/3.插入数据.py
@@ -8,11 +8,31 @@ age = 20
 db = pymysql.connect(host='127.0.0.1',port=3306,user='root',password='970202',db='pachong_test')
 cursor = db.cursor()
 
-sql = 'insert into students(id, name, age) values(%s,%s,%s)'
+
+data = {
+    'id':'20180002',
+    'name':'Hamdi',
+    'age':25
+}
+table = 'students'
+keys = ','.join(data.keys())
+values = ','.join(['%s'] * len(data))
+sql = 'INSERT INTO {table}({keys}) VALUES ({values})'.format(table=table, keys=keys, values=values)
 try:
-    cursor.execute(sql,(id, user, age))
-    db.commit()
+    if cursor.execute(sql, tuple(data.values())):
+        print('Successful')
+        db.commit()
 except:
+    print('Failed')
     db.rollback()
 
 db.close()
+
+# sql = 'insert into students(id, name, age) values(%s,%s,%s)'
+# try:
+#     cursor.execute(sql,(id, user, age))
+#     db.commit()
+# except:
+#     db.rollback()
+#
+# db.close()
