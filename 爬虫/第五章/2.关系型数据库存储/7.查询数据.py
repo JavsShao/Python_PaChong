943a998e1a99de0c674e8a8560b76ebf7568a468
hamdi <javs_shao@163.com>
2018-11-26 13:22:15 +0800

    连接数据的条数

--- 爬虫/第五章/2.关系型数据库存储/7.查询数据.py
+++ 爬虫/第五章/2.关系型数据库存储/7.查询数据.py
@@ -3,3 +3,18 @@ import pymysql
 
 # 链接数据库
 db = pymysql.connect(host='localhost',port=3306,user='root',password='970202',db='pachong_test')
+# 获取游标
+cursor = db.cursor()
+
+# 查询语句
+sql = 'select * from students where age >= 20'
+
+try:
+    cursor.execute(sql)
+    # 计算数据
+    print('Count:',cursor.rowcount)
+    one = cursor.fetchone()
+    # print('One:',one)
+    # result = cursor.fetchall()
+except:
+    print("出现错误！")
