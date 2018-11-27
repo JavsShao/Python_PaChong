48321abfac43579c07e317a3e43cd4a22d0479c2
hamdi <javs_shao@163.com>
2018-11-27 10:59:41 +0800

    插入数据

--- 爬虫/第五章/3.Mongodb/1.连接MongoDB.py
+++ 爬虫/第五章/3.Mongodb/1.连接MongoDB.py
@@ -2,4 +2,28 @@ import pymongo
 
 
 client = pymongo.MongoClient(host='localhost',port=27017)
-print('连接成功！')
+# print('连接成功！')
+
+db = client.students_1
+
+collection = db.students
+
+# 插入数据
+student = {
+    'id':'20180001',
+    'name':'Hamdi',
+    'age':25,
+    'gender':'male'
+
+}
+
+student2 = {
+    'id':'20180002',
+    'name':'Mike',
+    'age':20,
+    'gender':'male'
+}
+
+result = collection.insert(student,student2)
+print(result)
+
