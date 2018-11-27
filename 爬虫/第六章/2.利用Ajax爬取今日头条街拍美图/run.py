e33343fc78aacf2171be20c944f56dca0b296114
hamdi <javs_shao@163.com>
2018-11-27 17:25:46 +0800

    保存图片

--- 爬虫/第六章/2.利用Ajax爬取今日头条街拍美图/run.py
+++ 爬虫/第六章/2.利用Ajax爬取今日头条街拍美图/run.py
@@ -1,6 +1,8 @@
 import requests
+import os
 
 from urllib.parse import urlencode
+from hashlib import md5
 
 
 # 获取页面信息
@@ -35,3 +37,19 @@ def get_images(json):
                     'image':'http:' + image.get('url'),
                     'title':title
                 }
+
+# 保存图片
+def save_image(item):
+    if not os.path.exists(item.get('title')):
+        os.mkdir(item.get('title'))
+    try:
+        response = requests.get(item.get('image'))
+        if response.status_code == 200:
+            file_path = '{0}/{1}.{2}'.format(item.get('title'), md5(response.content).hexdigest(), 'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path, "wb") as f:
+                    f.write(response.content)
+            else:
+                print("下载完成！",file_path)
+    except requests.ConnectionError:
+        print('图片下载失败！')
