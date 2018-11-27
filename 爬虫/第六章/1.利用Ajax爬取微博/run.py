21ff8b1c7fe51e635e230401581bd07ca7ed446a
hamdi <javs_shao@163.com>
2018-11-27 15:15:10 +0800

    成功爬取微博内容

--- 爬虫/第六章/1.利用Ajax爬取微博/run.py
+++ 爬虫/第六章/1.利用Ajax爬取微博/run.py
@@ -1,51 +1,57 @@
-# 微博开头网址
-from urllib.parse import urlencode
-
 import requests
+from urllib.parse import urlencode
+from pyquery import PyQuery as pq
+from pymongo import MongoClient
 
 base_url = 'https://m.weibo.cn/api/container/getIndex?'
-
-# 请求头信息
 headers = {
-    # 访问的主机地址
-    'Host':'m.weibo.cn',
-    # 重定向地址
-    'Referer':'https://m.weibo.cn/u/3024395081',
-    # 浏览器 & 操作系统信息
+    'Host': 'm.weibo.cn',
+    'Referer': 'https://m.weibo.cn/u/2830678474',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    # Ajax请求
     'X-Requested-With': 'XMLHttpRequest',
 }
+client = MongoClient()
+db = client['weibo']
+collection = db['weibo']
+max_page = 10
 
-# 获取请求的json结果
+
 def get_page(page):
-    # 请求的参数
     params = {
-        'type':'uid',
-        'value':'3024395081',
-        'containerid':'1005053024395081',
-        'page':page
+        'type': 'uid',
+        'value': '3024395081',
+        'containerid': '1076033024395081',
+        'page': page
     }
-    # 访问的地址
     url = base_url + urlencode(params)
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            return response.json()
+            return response.json(), page
     except requests.ConnectionError as e:
-        print('错误！',e.args)
+        print('Error', e.args)
 
-# 解析Json结果
-def parse_page(json):
+
+def parse_page(json, page: int):
     if json:
-        # cards在data参数下面
         items = json.get('data').get('cards')
-        for item in items:
-            item = item.get('mblog')
-            weibo = {}
-            weibo['id'] = item.get('id')
-            weibo['text'] = item.get('text')
-            weibo['attitudes'] = item.get('attitudes')
-            weibo['comments'] = item.get('comment_count')
-            weibo['reposts'] = item.get('reposts_count')
-            yield weibo
+        for index, item in enumerate(items):
+            if page == 1 and index == 1:
+                continue
+            else:
+                item = item.get('mblog', {})
+                weibo = {}
+                weibo['id'] = item.get('id')
+                weibo['内容'] = pq(item.get('text')).text()
+                weibo['点赞数'] = item.get('attitudes_count')
+                weibo['评论数'] = item.get('comments_count')
+                weibo['转发量'] = item.get('reposts_count')
+                yield weibo
+
+
+if __name__ == '__main__':
+    for page in range(1, max_page + 1):
+        json = get_page(page)
+        results = parse_page(*json)
+        for result in results:
+            print(result)
