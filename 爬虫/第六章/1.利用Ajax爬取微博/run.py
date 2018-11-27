6214150c4c889cc60c785b5840b3886c7468cb3d
hamdi <javs_shao@163.com>
2018-11-27 14:16:00 +0800

    请求头信息

--- 爬虫/第六章/1.利用Ajax爬取微博/run.py
+++ 爬虫/第六章/1.利用Ajax爬取微博/run.py
@@ -1,2 +1,14 @@
 # 微博开头网址
 base_url = 'https://m.weibo.cn/api/container/getIndex?'
+
+# 请求头信息
+headers = {
+    # 访问的主机地址
+    'Host':'m.weibo.cn',
+    # 重定向地址
+    'Referer':'https://m.weibo.cn/u/3024395081',
+    # 浏览器 & 操作系统信息
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    # Ajax请求
+    'X-Requested-With': 'XMLHttpRequest',
+}
