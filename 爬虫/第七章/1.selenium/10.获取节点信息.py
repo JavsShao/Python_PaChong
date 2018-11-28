ae0689543e92c0c43b04837cd4d9a42ed01e0e52
hamdi <javs_shao@163.com>
2018-11-28 15:02:52 +0800

    获取id、位置、标签名和大小

--- 爬虫/第七章/1.selenium/10.获取节点信息.py
+++ 爬虫/第七章/1.selenium/10.获取节点信息.py
@@ -15,10 +15,25 @@ from selenium.webdriver import ActionChains
 # print(logo.get_attribute('class'))
 
 # 获取文本值
+# browser = webdriver.Chrome()
+# url = 'https://www.zhihu.com/explore'
+# browser.get(url)
+# input = browser.find_element_by_class_name('zu-top-add-question')
+# print(input.text)
+# input = browser.find_element_by_class_name('hide-text')
+# print(input.text)
+
+# 获取id，位置，标签名和大小
 browser = webdriver.Chrome()
 url = 'https://www.zhihu.com/explore'
 browser.get(url)
 input = browser.find_element_by_class_name('zu-top-add-question')
-print(input.text)
-input = browser.find_element_by_class_name('hide-text')
-print(input.text)
+# id属性
+print(input.id)
+# 相对位置
+print(input.location)
+# 获取标签名称
+print(input.tag_name)
+# 节点大小
+print(input.size)
+browser.close()
