9edf5657dec1a43642bbf44581c7a2a34c2d7c88
hamdi <javs_shao@163.com>
2018-11-28 15:45:45 +0800

    选项卡管理

---
+++ 爬虫/第七章/1.selenium/15.选项卡管理.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+
+
+browser = webdriver.Chrome()
+browser.get('https://www.baidu.com')
+# 开启新的选项卡
+browser.execute_script('window.open()')
+# 调用weindow_handles属性获取当前开启的所有选项卡，返回的是选项卡的代号列表
+print(browser.window_handles)
+# 切换选项卡
+browser.switch_to_window(browser.window_handles[1])
+browser.get('https://www.taobao.com')
+time.sleep(3)
+# 切换选项卡
+browser.switch_to_window(browser.window_handles[0])
+browser.get('https://python.org')
