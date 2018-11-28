6fe8b30c8b02afa42caa9fd7b58d3fa581ea2e9a
hamdi <javs_shao@163.com>
2018-11-28 11:36:17 +0800

    节点互动

--- 爬虫/第七章/1.selenium/7.节点交互.py
+++ 爬虫/第七章/1.selenium/7.节点交互.py
@@ -10,7 +10,7 @@ time.sleep(1)
 input.clear()
 input.send_keys('ipad')
 
-# 因为淘宝网的搜索按钮下的div只有这一个功能，所以btn-search 和 search-button功能一样，但是规范用起来应该是btn-search
+# 因为淘宝网的搜索按钮下的div只有这一个功能，所以btn-search 和 search-button功能一样，但是规范用起来应该是btn-search0p
 # button = browser.find_element_by_class_name('btn-search')
 button = browser.find_element_by_class_name('search-button')
 button.click()
