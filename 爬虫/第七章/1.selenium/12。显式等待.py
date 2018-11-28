f91642d734ad5ca57a252a62ea0583d671c5334b
hamdi <javs_shao@163.com>
2018-11-28 15:19:50 +0800

    显式等待_2

--- 爬虫/第七章/1.selenium/12。显式等待.py
+++ 爬虫/第七章/1.selenium/12。显式等待.py
@@ -6,7 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome()
 browser.get('https://www.taobao.com')
-wait = WebDriverWait(browser, 10)
+# 引入WebDriverWait对象，指定最长等待时间
+wait = WebDriverWait(browser, 1)
+
 input = wait.until(EC.presence_of_element_located((By.ID, 'q')))
 button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.btn-search')))
 print(input, button)
