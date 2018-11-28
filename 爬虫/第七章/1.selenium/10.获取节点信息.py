59ddbe0175b16ae0fed1bfa52a505736fb088102
hamdi <javs_shao@163.com>
2018-11-28 14:56:01 +0800

    获取文本值

--- 爬虫/第七章/1.selenium/10.获取节点信息.py
+++ 爬虫/第七章/1.selenium/10.获取节点信息.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 
+# 获取属性
+# browser = webdriver.Chrome()
+# url = 'https://www.zhihu.com/explore'
+# browser.get(url)
+# logo = browser.find_element_by_id('zh-top-link-logo')
+# print(logo)
+# print(logo.get_attribute('class'))
+#
+# print('-----分隔符------')
+# logo = browser.find_element_by_class_name('zu-top-link-logo')
+# print(logo)
+# print(logo.get_attribute('class'))
 
+# 获取文本值
 browser = webdriver.Chrome()
 url = 'https://www.zhihu.com/explore'
 browser.get(url)
-logo = browser.find_element_by_id('zh-top-link-logo')
-print(logo)
-print(logo.get_attribute('class'))
-
-print('-----分隔符------')
-logo = browser.find_element_by_class_name('zu-top-link-logo')
-print(logo)
-print(logo.get_attribute('class'))
+input = browser.find_element_by_class_name('zu-top-add-question')
+print(input.text)
+input = browser.find_element_by_class_name('hide-text')
+print(input.text)
