8f8e655f8bd8c233954760bb94aa551759031fb4
hamdi <javs_shao@163.com>
2018-11-28 12:12:22 +0800

    获取知乎发现的logo属性_获取属性

---
+++ 爬虫/第七章/1.selenium/10.获取节点信息.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+
+browser = webdriver.Chrome()
+url = 'https://www.zhihu.com/explore'
+browser.get(url)
+logo = browser.find_element_by_id('zh-top-link-logo')
+print(logo)
+print(logo.get_attribute('class'))
+
+print('-----分隔符------')
+logo = browser.find_element_by_class_name('zu-top-link-logo')
+print(logo)
+print(logo.get_attribute('class'))
