3eed444bcc85e42bc350ba66381ae66caffdd18f
hamdi <javs_shao@163.com>
2018-11-28 19:23:39 +0800

    抓取索引页

--- 爬虫/第七章/淘宝/run.py
+++ 爬虫/第七章/淘宝/run.py
@@ -12,3 +12,29 @@ wait = WebDriverWait(browser, 10)
 # 要搜索的商品
 KEYWORD = 'iPad'
 
+def index_page(page):
+    '''
+    抓取索引页
+    :param page: 页数
+    :return:
+    '''
+    print('正在爬取第', page, '页')
+    try:
+        url = 'https://s.taobao.com/search?q' + quote(KEYWORD)
+        browser.get(url)
+        if page > 1:
+            input = wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input')))
+            submit = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
+            input.clear()
+            input.send_keys(page)
+            submit.click()
+        wait.until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active > span'), str(page)))
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
+        get_products()
+    except TimeoutException:
+        index_page(page)
+
+def get_products():
