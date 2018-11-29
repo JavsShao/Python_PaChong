fcfab055821bf41df57b8422abc1e4386554592c
hamdi <javs_shao@163.com>
2018-11-29 15:37:37 +0800

    execute_执行Lua脚本

--- 爬虫/第七章/3.Splash/6.execute.py
+++ 爬虫/第七章/3.Splash/6.execute.py
@@ -2,12 +2,28 @@ import requests
 from urllib.parse import quote
 
 
+# lua = '''
+# function main(splash)
+#     return 'hello'
+# end
+# '''
+#
+# url = 'http://localhost:8050/excute?lua_source=' + quote(lua)
+# response = requests.get(url)
+# print(response.text)
+
 lua = '''
-function main(splash)
-    return 'hello'
+function main(splash, args)
+    local treat=require('treat')
+    local response=splash:http_get("http://httpbin.org/get")
+    return {
+        html=treat.as_string(response.body),
+        url=response.url,
+        status=response.status
+    }
 end
 '''
 
-url = 'http://localhost:8050/excute?lua_source=' + quote(lua)
+url = 'http://localhost:8050/execute?lua_source=' + quote(lua)
 response = requests.get(url)
 print(response.text)
