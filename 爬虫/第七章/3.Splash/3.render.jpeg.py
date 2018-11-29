eaa85724776336514f885be3110976e2b0321a1a
hamdi <javs_shao@163.com>
2018-11-29 15:01:07 +0800

    render.jpeg

---
+++ 爬虫/第七章/3.Splash/3.render.jpeg.py
@@ -0,0 +1,7 @@
+import requests
+
+
+url = 'http://localhost:8050/render.jpeg?url=https://www.jd.com&wait=5&width=1000&height=700'
+response = requests.get(url)
+with open('jd.jpeg','wb') as f:
+    f.write(response.content)
