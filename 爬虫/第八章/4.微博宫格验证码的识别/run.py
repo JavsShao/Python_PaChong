1a79da4884b0849bc99f095619260127b744f8b3
hamdi <javs_shao@163.com>
2018-11-30 22:55:16 +0800

    初始化

--- 爬虫/第八章/4.微博宫格验证码的识别/run.py
+++ 爬虫/第八章/4.微博宫格验证码的识别/run.py
@@ -1,4 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
 USERNAME = 'Hamdi__'
 PASSWORD = 'QWERTY0202'
 
 TEMPLATES_FOLDER = 'templates'
+
+class CrackWeiboSlide():
+    def __init__(self):
+        self.url = 'https://passport.weibo.cn/signin/login?entry=mweibo&r=https://m.weibo.cn'
+        self.browser = webdriver.Chrome()
+        self.wait = WebDriverWait(self.browser, 20)
+        self.username = USERNAME
+        self.password = PASSWORD
