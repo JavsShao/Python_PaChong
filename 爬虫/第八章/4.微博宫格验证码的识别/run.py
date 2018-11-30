4a4adbee82ae4e371f8d51ea50f9c258582d4394
hamdi <javs_shao@163.com>
2018-11-30 22:56:19 +0800

    初始化

--- 爬虫/第八章/4.微博宫格验证码的识别/run.py
+++ 爬虫/第八章/4.微博宫格验证码的识别/run.py
@@ -8,6 +8,9 @@ TEMPLATES_FOLDER = 'templates'
 
 class CrackWeiboSlide():
     def __init__(self):
+        '''
+        初始化
+        '''
         self.url = 'https://passport.weibo.cn/signin/login?entry=mweibo&r=https://m.weibo.cn'
         self.browser = webdriver.Chrome()
         self.wait = WebDriverWait(self.browser, 20)
