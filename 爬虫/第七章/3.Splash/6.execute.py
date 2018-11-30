bc4045d4a54c5b8405a1134cb0a2ce9422afce44
hamdi <javs_shao@163.com>
2018-11-30 11:25:22 +0800

    execute_执行Lua脚本

--- 爬虫/第七章/3.Splash/6.execute.py
+++ 爬虫/第七章/3.Splash/6.execute.py
@@ -24,6 +24,6 @@ function main(splash, args)
 end
 '''
 
-url = 'http://localhost:8050/execute?lua_source=' + quote(lua)
+url = 'http:/ki/localhost:8050/execute?lua_source=' + quote(lua)
 response = requests.get(url)
 print(response.text)
