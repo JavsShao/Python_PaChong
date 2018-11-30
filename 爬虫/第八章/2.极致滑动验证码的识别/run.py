97e4132d883edf1a8bd881bef9742d0569e20ce1
hamdi <javs_shao@163.com>
2018-11-30 12:08:12 +0800

    机制滑动验证码_初始化

--- 爬虫/第八章/2.极致滑动验证码的识别/run.py
+++ 爬虫/第八章/2.极致滑动验证码的识别/run.py
@@ -1,6 +1,16 @@
-
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 # 配置
 EMAIL = 'javs_shao@163.com'
 PASSWORD = 'QWERTY1234'
+
+class CrackGeetest():
+    def __init__(self):
+        self.url = 'https://account.geetest.com/login'
+        self.browser = webdriver.Chrome()
+        self.wait = WebDriverWait
+        self.emaile = EMAIL
+        self.password = PASSWORD
