87cee93692ceb5d342d31bb65d19c98c18cdecc8
hamdi <javs_shao@163.com>
2018-11-30 22:18:07 +0800

    代码整合

--- 爬虫/第八章/3.点触验证码的识别/run.py
+++ 爬虫/第八章/3.点触验证码的识别/run.py
@@ -1,124 +1,115 @@
 import time
 from io import BytesIO
-
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from .captach import Chaojiying
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from captach import Chaojiying
 
-Email = 'javs_shao@163.com'
-PASSWORD = '111'
+EMAIL = 'javs_shao@163.com'
+PASSWORD = ''
 
 CHAOJIYING_USERNAME = 'Hamdi0202'
-CHAOJIYING_PASSWORD = 'qwerty1234'
+CHAOJIYING_PASSWORD = ''
 CHAOJIYING_SOFT_ID = 897939
 CHAOJIYING_KIND = 2002
 
 
-class CrackTouClick(object):
-
+class CrackTouClick():
     def __init__(self):
-        '''
-        初始化
-        '''
-        self.url = 'http:admin.touclick.com/login.html'
+        self.url = 'http://admin.touclick.com/login.html'
         self.browser = webdriver.Chrome()
         self.wait = WebDriverWait(self.browser, 20)
-        self.email = Email
+        self.email = EMAIL
         self.password = PASSWORD
-        self.chaojiying = Chaojiying(CHAOJIYING_USERNAME, CHAOJIYING_PASSWORD,CHAOJIYING_SOFT_ID)
+        self.chaojiying = Chaojiying(CHAOJIYING_USERNAME, CHAOJIYING_PASSWORD, CHAOJIYING_SOFT_ID)
 
     def __del__(self):
-        '''
-        关闭浏览器
-        :return:
-        '''
         self.browser.close()
 
     def open(self):
-        '''
-        打开网页输入用户名和密码（填写表单）
-        :return:
-        '''
-        self.browser(self.url)
-        email = self.wait.until(EC.presence_of_element_located((By.ID,'email')))
+        """
+        打开网页输入用户名密码
+        :return: None
+        """
+        self.browser.get(self.url)
+        email = self.wait.until(EC.presence_of_element_located((By.ID, 'email')))
         password = self.wait.until(EC.presence_of_element_located((By.ID, 'password')))
         email.send_keys(self.email)
-        email.send_keys(self.password)
+        password.send_keys(self.password)
 
     def get_touclick_button(self):
-        '''
+        """
         获取初始验证按钮
         :return:
-        '''
-        button = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'touclick-hod-wrap')))
+        """
+        button = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'touclick-hod-wrap')))
         return button
 
     def get_touclick_element(self):
-        '''
-        获取验证码图片对象
+        """
+        获取验证图片对象
         :return: 图片对象
-        '''
+        """
         element = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'touclick-pub-content')))
         return element
 
-    def get_screenshot(self):
-        '''
-        获取网页截图
-        :return:
-        '''
-        screenshot = self.browser.get_screenshot_as_png()
-        screenshot = Image.open(BytesIO(screenshot))
-        return screenshot
-
-    def get_touclick_image(self, name='captcha.png'):
-        '''
-        获取验证码图片
-        :param name:
-        :return: 图片对象
-        '''
-        top, bottom, left, right = self.get_position()
-        print('验证码位置：',top, bottom, left, right)
-        screenshot = self.get_screenshot()
-        captcha = screenshot.crop(left, top, right, bottom)
-        captcha.save(name)
-        return captcha
-
     def get_position(self):
-        '''
+        """
         获取验证码位置
         :return: 验证码位置元组
-        '''
+        """
         element = self.get_touclick_element()
         time.sleep(2)
         location = element.location
         size = element.size
         top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], location['x'] + size[
             'width']
-        return {top, bottom, left, right}
+        return (top, bottom, left, right)
+
+    def get_screenshot(self):
+        """
+        获取网页截图
+        :return: 截图对象
+        """
+        screenshot = self.browser.get_screenshot_as_png()
+        screenshot = Image.open(BytesIO(screenshot))
+        return screenshot
+
+    def get_touclick_image(self, name='captcha.png'):
+        """
+        获取验证码图片
+        :return: 图片对象
+        """
+        top, bottom, left, right = self.get_position()
+        print('验证码位置', top, bottom, left, right)
+        screenshot = self.get_screenshot()
+        captcha = screenshot.crop((left, top, right, bottom))
+        captcha.save(name)
+        return captcha
 
     def get_points(self, captcha_result):
-        '''
+        """
         解析识别结果
         :param captcha_result: 识别结果
         :return: 转化后的结果
-        '''
+        """
         groups = captcha_result.get('pic_str').split('|')
         locations = [[int(number) for number in group.split(',')] for group in groups]
         return locations
 
     def touch_click_words(self, locations):
-        '''
+        """
         点击验证图片
         :param locations: 点击位置
-        :return:
-        '''
+        :return: None
+        """
         for location in locations:
             print(location)
-            ActionChains(self.browser).move_to_element_with_offset(self.get_touclick_element(), location[0],location[1].click().perform)
+            ActionChains(self.browser).move_to_element_with_offset(self.get_touclick_element(), location[0],
+                                                                   location[1]).click().perform()
             time.sleep(1)
 
     def touch_click_verify(self):
@@ -130,14 +121,14 @@ class CrackTouClick(object):
         button.click()
 
     def login(self):
-        '''
+        """
         登录
-        :return:None
-        '''
+        :return: None
+        """
         submit = self.wait.until(EC.element_to_be_clickable((By.ID, '_submit')))
         submit.click()
         time.sleep(10)
-        print('登录成功！')
+        print('登录成功')
 
     def crack(self):
         """
@@ -168,6 +159,7 @@ class CrackTouClick(object):
             self.crack()
         else:
             self.login()
+
 
 if __name__ == '__main__':
     crack = CrackTouClick()
