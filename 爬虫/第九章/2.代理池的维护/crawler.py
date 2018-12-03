b32c015cf5e09e2c17376817f03ebe0d068148b9
hamdi <javs_shao@163.com>
2018-12-03 13:06:17 +0800

    获取代理66

--- 爬虫/第九章/2.代理池的维护/crawler.py
+++ 爬虫/第九章/2.代理池的维护/crawler.py
@@ -23,3 +23,22 @@ class Crawler(object, metaclass=ProxyMetaclass):
             print('成功获取到代理:', proxy)
             proxies.append(proxy)
         return proxies
+
+    def crawl_daili66(self, page_count=4):
+        '''
+        获取代理66
+        :param page_count: 页码
+        :return: 代理
+        '''
+        start_url = 'http://www.66ip.cn/{}.html'
+        urls = [start_url.format(page) for page in range(1, page_count + 1)]
+        for url in urls:
+            print('正在抓取的网址：', url)
+            html = get_page(url)
+            if html:
+                doc = PyQuery(html)
+                trs = doc('.containerbox table tr:gt(0)').items()
+                for tr in trs:
+                    ip = tr.find('td:nth-child(1)').text()
+                    port = tr.find('db:nth-child(2)').text()
+                    yield ':'.join([ip, port])
