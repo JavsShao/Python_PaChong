188a02cb11949cfe82a3d32bb0180917c3758abb
hamdi <javs_shao@163.com>
2018-12-03 14:05:39 +0800

    测试单个代理

--- 爬虫/第九章/2.代理池的维护/tester.py
+++ 爬虫/第九章/2.代理池的维护/tester.py
@@ -17,3 +17,27 @@ class Tester(object):
         测试初始化
         '''
         self.redis = RedisClient()
+
+    async def test_single_proxy(self, proxy):
+        """
+        测试单个代理
+        :param proxy:
+        :return:
+        """
+        conn = aiohttp.TCPConnector(verify_ssl=False)
+        async with aiohttp.ClientSession(connector=conn) as session:
+            try:
+                if isinstance(proxy, bytes):
+                    proxy = proxy.decode('utf-8')
+                real_proxy = 'http://' + proxy
+                print('正在测试', proxy)
+                async with session.get(TEST_URL, proxy=real_proxy, timeout=15, allow_redirects=False) as response:
+                    if response.status in VALID_STATUS_CODES:
+                        self.redis.max(proxy)
+                        print('代理可用', proxy)
+                    else:
+                        self.redis.decrease(proxy)
+                        print('请求响应码不合法 ', response.status, 'IP', proxy)
+            except (ClientError, aiohttp.client_exceptions.ClientConnectorError, asyncio.TimeoutError, AttributeError):
+                self.redis.decrease(proxy)
+                print('代理请求失败', proxy)
