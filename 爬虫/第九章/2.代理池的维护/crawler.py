5243ff2f610f763ecc8333347900a388dc3e3e8d
hamdi <javs_shao@163.com>
2018-12-03 12:41:29 +0800

    成功获取到代理_提示信息

--- 爬虫/第九章/2.代理池的维护/crawler.py
+++ 爬虫/第九章/2.代理池的维护/crawler.py
@@ -14,3 +14,12 @@ class ProxyMetaclass(type):
                 count += 1
         attrs['__CrawlFuncCount__'] = count
         return type.__new__(cls, name, bases, attrs)
+
+class Crawler(object, metaclass=ProxyMetaclass):
+
+    def get_proxies(self, callback):
+        proxies = []
+        for proxy in eval('self.{}()'.format(callback)):
+            print('成功获取到代理:', proxy)
+            proxies.append(proxy)
+        return proxies
