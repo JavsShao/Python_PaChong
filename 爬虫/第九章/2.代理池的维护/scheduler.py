d1c6f25affc6c979254b51b1b9a7b26744da95c8
hamdi <javs_shao@163.com>
2018-12-03 14:21:13 +0800

    定时获取代理

--- 爬虫/第九章/2.代理池的维护/scheduler.py
+++ 爬虫/第九章/2.代理池的维护/scheduler.py
@@ -1,7 +1,7 @@
 import time
 from multiprocessing import Process
 
-from settings import TESTER_CYCLE
+from settings import *
 from api import app
 from getter import Getter
 from tester import Tester
@@ -19,4 +19,14 @@ class Scheluler(object):
         while True:
             print('测试器开始运行！')
             tester.run()
+            time.sleep(cycle)
+
+    def schedule_getter(self, cycle=GETTER_CYCLE):
+        """
+        定时获取代理
+        """
+        getter = Getter()
+        while True:
+            print('开始抓取代理')
+            getter.run()
             time.sleep(cycle)
