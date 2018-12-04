1751a7855f59a68836526826200b56a1842d2f18
hamdi <javs_shao@163.com>
2018-12-04 10:54:06 +0800

    Redis队列模块入口

--- 爬虫/第九章/3.代理池_微信公众号/weixin/db_redis.py
+++ 爬虫/第九章/3.代理池_微信公众号/weixin/db_redis.py
@@ -45,3 +45,13 @@ class RedisQueue():
         '''
         return self.db.llen(REDIS_KEY) == 0
 
+if __name__ == '__main__':
+    '''Redis队列模块入口'''
+    db = RedisQueue()
+    start_url = 'https://www.baidu.com'
+    weixin_request = WeixinRequest(url=start_url, callback='hello', need_proxy=True)
+    db.add(weixin_request)
+    request = db.pop()
+    print(request)
+    print(request.callback, request.need_proxy)
+
