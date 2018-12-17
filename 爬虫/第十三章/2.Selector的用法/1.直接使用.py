e9a320bbddbe4e195960992f0e1262c7f01d87c2
hamdi <javs_shao@163.com>
2018-12-17 13:11:57 +0800

    Selector_直接使用

---
+++ 爬虫/第十三章/2.Selector的用法/1.直接使用.py
@@ -0,0 +1,13 @@
+from scrapy import Selector
+
+
+body = '<html><head><title>Hello World</title></head><body></body></html>'
+selector = Selector(text=body)
+title = selector.xpath('//title/text()').extract_first()
+print(title)
+
+title = selector.xpath('//title/text()')
+print(title)
+
+title = selector.xpath('//title/text').extract()
+print(title)
